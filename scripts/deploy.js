70909e81c1e34d6cb800bb9a051d6c4e7df5cd37
xedk <digvijaysra@gmail.com>
2021-12-20 01:26:20 +0530

    deploy.js populated with contract params

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -6,7 +6,7 @@ const main = async () => {
     console.log("Account balance:", (await deployer.getBalance()).toString());
 
     const Web3Gifts = await ethers.getContractFactory("Web3GiftsNFT");
-    const web3gifts = await Web3Gifts.deploy();
+    const web3gifts = await Web3Gifts.deploy("Web3Gifts", "W3G", "ipfs://QmXqUMkNLRdpjHfwoAomzXPz9HfTLMZVsnjsX9HwbHJaBe");
 
     console.log("Contract address:", web3gifts.address);
 }
